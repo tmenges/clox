c2ae5fcf9cfe158ea53a3e8003b2335e660493e4
Tony Menges <tony.menges@gmail.com>
2024-06-11 11:41:04 -0700

    Chapter 14

---
+++ debug.h
@@ -0,0 +1,9 @@
+#ifndef LOX_DEBUG_H
+#define LOX_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif //LOX_DEBUG_H
---
+++ value.h
@@ -0,0 +1,19 @@
+#ifndef LOX_VALUE_H
+#define LOX_VALUE_H
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
+#endif //LOX_VALUE_H
