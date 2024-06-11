10341a8f2df12c7260a72d0fe841375094560e04
Tony Menges <tony.menges@gmail.com>
2024-06-11 15:56:42 -0700

    Chapter 15

---
+++ vm.h
@@ -0,0 +1,28 @@
+#ifndef LOX_VM_H
+#define LOX_VM_H
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk* chunk;
+    uint8_t* ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk* chunk);
+void push(Value vale);
+Value pop();
+
+#endif //LOX_VM_H
