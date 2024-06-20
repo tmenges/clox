dad86e1519ae2fb38543f3a0b3e23440b8c35f42
Tony Menges <tony.menges@gmail.com>
2024-06-20 10:55:26 -0700

    Chapter 20

--- value.h
+++ value.h
@@ -32,9 +32,9 @@ typedef struct {
 #define AS_OBJ(value)           ((value).as.obj)
 
 #define BOOL_VAL(value)         ((Value) {VAL_BOOL, {.boolean = value}})
-#define NIL_VAL(value)          ((Value) {VAL_NIL, {.number = 0}})
+#define NIL_VAL                 ((Value) {VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value)       ((Value) {VAL_NUMBER, {.number = value}})
-#define OBJ_VAL(object)          ((Value) {VAL_OBJ, {.obj = (Obj*)object}})
+#define OBJ_VAL(object)         ((Value) {VAL_OBJ, {.obj = (Obj*)object}})
 
 typedef struct {
     int capacity;
--- vm.c
+++ vm.c
@@ -30,10 +30,13 @@ static void runtimeError(const char* format, ...) {
 
 void initVM() {
     resetStack();
+    vm.objects = NULL;
+    initTable(&vm.strings);
 }
 
 void freeVM() {
-   freeObjects();
+    freeTable(&vm.strings);
+    freeObjects();
 }
 
 void push(Value value) {
@@ -101,7 +104,7 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
-            case OP_NIL:       push(NIL_VAL()); break;
+            case OP_NIL:       push(NIL_VAL); break;
             case OP_TRUE:      push(BOOL_VAL(true)); break;
             case OP_FALSE:     push(BOOL_VAL(false)); break;
             case OP_EQUAL: {
